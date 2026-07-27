a8768955624230c9186b1ece26b4821bdcc8f521
Josiah Kennedy <abkennedyj@gmail.com>
2026-07-27 20:14:46 +0000

    Update registry path and add Secure Boot requirement

---
+++ WN11-CC-000070.ps1
@@ -0,0 +1,42 @@
+<#
+.SYNOPSIS
+    Command line data must be included in process creation events.
+    
+.NOTES
+    Author          : JosiahKennedy
+    LinkedIn        : linkedin.com/in/josiahtakyi/
+    GitHub          : github.com/abkennedyj
+    Date Created    : 2026-07-27
+    Last Modified   : 2024-07-27
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-CC-000070
+    Documentation   : https://stigaview.com/products/win11/v2r7/WN11-CC-000070/
+
+.TESTED ON
+    Date(s) Tested  : 2026-07-27
+    Tested By       : Josiah Kennedy
+    Systems Tested  : Windows 11 
+    PowerShell Ver. : 5.1
+
+.USAGE
+    Run script As Admin in PowerShell 
+    PS C:\> .\WN11-CC-000070.ps1
+#>
+
+
+# Registry path and value name
+$Path = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DeviceGuard"
+$Name = "RequirePlatformSecurityFeatures"
+
+# Create the registry key if it doesn't exist
+if (-not (Test-Path $Path)) {
+    New-Item -Path $Path -Force | Out-Null
+}
+
+# Require Secure Boot
+New-ItemProperty -Path $Path -Name $Name -Value 1 -PropertyType DWord -Force | Out-Null
+
+# Verify the setting
+Get-ItemProperty -Path $Path -Name $Name
